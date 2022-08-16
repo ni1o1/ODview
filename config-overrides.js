83cae0c1bf8dc059fa82637cc584736c7fb05369
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-08-16 10:06:46 +0800

    update package.json

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
+const path = require("path");
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css',
+    }),
+    addWebpackAlias({
+        '@': path.resolve(__dirname, 'src'),
+        '@@': path.resolve(__dirname, 'src/components'),
+    })
+);
